0e0dd32e82da2e76a0c63cdc0103d823695653fa
Benaiah Ambie-Barango <bennieb96@gmail.com>
2020-03-23 14:25:18 -0400

    api complete

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -6,30 +6,33 @@ $(document).ready(function () {
 });
 
 function validateUrl(value) {
-    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+    return /^(https?:\/\/)(([\da-z\.-]+)\.)?([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(value)
 }
 
 async function handleSubmit() {
     event.preventDefault()
     let formText = $('#url').val()
-    // if (validateUrl(formText)) {
+    if (validateUrl(formText)) {
+        console.log(`::: Form Submitted ::: ${formText}`)
+        let formId = $(this).attr('id')
 
-    console.log(`::: Form Submitted ::: ${formText}`)
-    let formId = $(this).attr('id')
-
-    // let newsInfo = 
-    await getNLP(formText, formId)
-    await postInfo(formId)
-    // }
-    // else {
-    // alert("Please enter a valid URL")
-    // }
+        try {
+            await getNLP(formText, formId)
+        } catch (error) {
+            console.log("error", error)
+        } 
+        await postInfo(formId)
+    }
+    else {
+        alert("Please enter a valid URL")
+    }
 }
 
 const getNLP = async (formText, formId) => {
     console.log("inside NLP promise")
     // if (formId == "classify") {
-        // console.log("inside sentiment")
+    // console.log("inside sentiment")
+    try {
         const request = await fetch(`/${formId}`, {
             method: 'POST',
             credentials: 'same-origin',
@@ -41,36 +44,43 @@ const getNLP = async (formText, formId) => {
                 "mode": "document"
             })
         })
-    // }
-    console.log("after promise")
+        // }
+        console.log("after promise")
+    } catch (error) {
+        console.log("error", error)
+    }
     // return await request.json();
     //add try-catch
 }
 
 const postInfo = async (formId) => {
     const request = await fetch('/form')
-    const news = await request.json()
-    // console.log(news)
-    $(document).ready(function () {
-        if (formId == "classify") {
-            $("#results").html(
-                `<p><b>Language</b>: ${news[news.length - 1].Lang} <br>
+    try {
+        const news = await request.json()
+        // console.log(news)
+        $(document).ready(function () {
+            if (formId == "classify") {
+                $("#results").html(
+                    `<p><b>Language</b>: ${news[news.length - 1].Lang} <br>
                 <b>Label</b>: ${news[0].Label}<br>
                 <b>Confidence</b>: ${news[news.length - 1].Conf}<br>
-                <b>Text</b>: ${news[news.length - 1].Text}<br>
+                <b>Text</b>: <span style="display:block;text-overflow: ellipsis;width: 400px;overflow: hidden; white-space: nowrap;">${news[news.length - 1].Text}</span><br>
                 </p>`
-            )
-        } else {
-            $("#results").html(
-                `<p><b>Title</b>: ${news[news.length - 1].Title} <br>
+                )
+            } else {
+                $("#results").html(
+                    `<p><b>Title</b>: ${news[news.length - 1].Title} <br>
                 <b>Author</b>: ${news[0].Author}<br>
                 <b>Publish Date</b>: ${news[news.length - 1].PublishDate}<br>
                 <b>Feeds</b>: ${news[news.length - 1].Feeds}<br>
                 </p>`
-            )
-        }
+                )
+            }
+        });
+    } catch (error) {
+        console.log("error", error)
+    }
 
-
-    });
 }
+
 // export { handleSubmit }
--- tests/newsTest.spec.js
+++ tests/newsTest.spec.js
@@ -1,19 +1,21 @@
-function filterByTerm(inputArr, searchTerm) {
-    return inputArr.filter(function(arrayElement) {
-      return arrayElement.url.match(searchTerm);
-    });
-  }
-  
-  describe("Filter function", () => {
-    test("it should filter by a search term (link)", () => {
-      const input = [
-        { id: 1, url: "https://www.url1.dev" },
-        { id: 2, url: "https://www.url2.dev" },
-        { id: 3, url: "https://www.link3.dev" }
-      ];
-  
-      const output = [{ id: 3, url: "https://www.link3.dev" }];
-  
-      expect(filterByTerm(input, "link")).toEqual(output);
-    });
-  });
+// const $ = require('jquery');
+const validateUrl = require('../src/client/js/index')
+
+test('should return true for a valid url', () => {
+  const text = validateUrl('http://techcrunch.com/2015/07/16/microsoft-will-never-give-up-on-mobile')
+  expect(text).toBe(true);
+})
+
+  // describe("Filter function", () => {
+  //   test("it should filter by a search term (link)", () => {
+  //     const input = [
+  //       { id: 1, url: "https://www.url1.dev" },
+  //       { id: 2, url: "https://www.url2.dev" },
+  //       { id: 3, url: "https://www.link3.dev" }
+  //     ];
+
+  //     const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+  //     expect(filterByTerm(input, "link")).toEqual(output);
+  //   });
+  // });
