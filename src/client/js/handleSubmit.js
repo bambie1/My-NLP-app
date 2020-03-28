435b872cc09e2a329f396441caa9fc861e00501a
Benaiah Ambie-Barango <bennieb96@gmail.com>
2020-03-28 12:13:47 -0400

    Test files included

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,6 +1,5 @@
-import {urlCheck} from './urlCheck'
-import {getNLP} from './getNLP'
-import {postInfo} from './postInfo'
+const fetch = require("node-fetch");
+import { urlCheck } from './urlCheck'
 
 async function handleSubmit() {
     event.preventDefault()
@@ -19,4 +18,51 @@ async function handleSubmit() {
     }
 }
 
-export {handleSubmit}
+async function getNLP (formText, formId) {
+    console.log("inside NLP promise")
+    const request = await fetch(`/${formId}`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "url": formText,
+            "mode": "document"
+        })
+    })
+    // }
+    console.log("after promise")
+    const reqJson = await request.json();
+    return reqJson;
+}
+
+const postInfo = async (formId) => {
+    const request = await fetch('/form')
+    const news = await request.json()
+    // console.log(news)
+    $(document).ready(function () {
+        if (formId == "classify") {
+            $("#results").html(
+                `<p><b>Language</b>: ${news[news.length - 1].Lang} <br>
+                <b>Label</b>: ${news[0].Label}<br>
+                <b>Confidence</b>: ${news[news.length - 1].Conf}<br>
+                <b>Text</b>: ${news[news.length - 1].Text}<br>
+                </p>`
+            )
+        } else {
+            $("#results").html(
+                `<p><b>Title</b>: ${news[news.length - 1].Title} <br>
+                <b>Author</b>: ${news[0].Author}<br>
+                <b>Publish Date</b>: ${news[news.length - 1].PublishDate}<br>
+                <b>Feeds</b>: ${news[news.length - 1].Feeds}<br>
+                </p>`
+            )
+        }
+    });
+}
+
+export { getNLP }
+// module.exports = {
+//     getNLP: getNLP
+// };
